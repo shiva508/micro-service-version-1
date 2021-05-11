9097014de0b93e9880d2dcc107399481fbac862c
shiva508 <dasarishiva1@gmail.com>
2021-05-11 14:01:22 +0530

    Config Server

---
+++ student-pool-cloud-config-service-v1/src/main/java/com/pool/StudentPoolCloudConfigServiceV1Application.java
@@ -0,0 +1,15 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class StudentPoolCloudConfigServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPoolCloudConfigServiceV1Application.class, args);
+	}
+
+}
---
+++ student-pool-cloud-config-service-v1/src/test/java/com/pool/StudentPoolCloudConfigServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPoolCloudConfigServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
