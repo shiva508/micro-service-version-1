1cadafae8152f35b745ef1960335185c9599543c
shiva508 <dasarishiva1@gmail.com>
2021-05-16 08:00:45 +0530

    spring cloud and config server

---
+++ student-pool-global-config-service/src/main/java/com/pool/StudentPoolGlobalConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPoolGlobalConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPoolGlobalConfigServiceApplication.class, args);
+	}
+
+}
---
+++ student-pool-global-config-service/src/main/java/com/pool/controller/GlobelConfigController.java
@@ -0,0 +1,24 @@
+package com.pool.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pool.model.GlobelConfigModel;
+import com.pool.model.StudentPoolConfig;
+
+@RestController
+public class GlobelConfigController {
+	
+	@Autowired
+	private StudentPoolConfig studentPoolConfig;
+
+	@GetMapping("/config")
+	public ResponseEntity<?> getGlobalConfigController() {
+		return new ResponseEntity<>(new GlobelConfigModel().setStudentName(studentPoolConfig.getStudentName()).setStudentRollNumber(studentPoolConfig.getStudentRollNumber()),
+				HttpStatus.OK);
+	}
+
+}
